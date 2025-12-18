d69c306dda1c42b943010d5e2a66076abf37df6b
Radin <radin44@protonmail.com>
2025-12-17 22:18:40 -0800

    added shell script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ask the user for their arduino board
+read -p "Enter your Arduino Port (e.g., /dev/ttyACM0): " PORT
+
+arduino-cli compile --fqbn arduino:avr:nano
+
+# Upload the code to the board
+arduino-cli upload -p $PORT --fqbn arduino:avr:nano
+
+# Open the Serial Monitor at baudrate of 115200 for user input
+arduino-cli monitor -p $PORT --config baudrate=115200

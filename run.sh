8bc41c5c76746f790980de14cf21064fa607794f
Radin <radin44@protonmail.com>
2025-12-18 17:39:30 -0800

    updated shell script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Ask the user for their arduino board
 read -p "Enter your Arduino Port (e.g., /dev/ttyACM0): " PORT
 
-arduino-cli compile --fqbn arduino:avr:nano
+arduino-cli compile --fqbn arduino:avr:nano --build-property "compiler.cpp.extra_flags=-Iinclude" Ultrasonic_Rover.ino
 
 # Upload the code to the board
 arduino-cli upload -p $PORT --fqbn arduino:avr:nano

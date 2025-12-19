cf66b637323dbbb6f3f2bc55393483ae34450fbd
Radin <radin44@protonmail.com>
2025-12-19 15:41:02 -0800

    updated code to average duration and stop after 5 seconds

--- Ultrasonic_Rover.ino
+++ Ultrasonic_Rover.ino
@@ -1,8 +1,11 @@
 #include "myheader.h"
 
-Servo myServo; // create servo object
-const int servoPin = 9;       // set servo pin
-const int obstacleLimit = 20; // limit at which to perform turns
+Servo myServo;   // create servo object
+const int servoPin{9};       // set servo pin
+const int obstacleLimit{20}; // distance threshold
+
+unsigned long forwardStartTime{};          // tracks forward motion start
+const unsigned long forwardTimeout{5000};   // 5 seconds
 
 void setup() {
   pinMode(ENA, OUTPUT);
@@ -17,7 +20,6 @@ void setup() {
 
   myServo.attach(servoPin);
   myServo.write(90); // center
-
   delay(1000);
 }
 
@@ -25,22 +27,41 @@ void loop() {
   int distance{};
   int leftDist{};
   int rightDist{};
-
+  
   distance = getDistance();
 
+  // forward logic
   if (distance > obstacleLimit) {
-    moveForward();
+
+    // Start moving forward if not already
+    if (forwardStartTime == 0) {
+      moveForward();
+      forwardStartTime = millis();
+    }
+
+    // Stop after 5 seconds max
+    if (millis() - forwardStartTime >= forwardTimeout) {
+      stopMotors();
+      forwardStartTime = 0; // reset
+    }
+
+    // Exit loop early; forward motion handled
+    return;
   } else {
     stopMotors();
-    delay(200);
+    forwardStartTime = 0; // reset in case it wasn't already
 
+    // Back up a bit
+    delay(200);
     moveBackward();
     delay(300);
     stopMotors();
 
+    // Look left and right
     leftDist = lookLeft();
     rightDist = lookRight();
 
+    // Decide which way to turn
     if (leftDist > rightDist) {
       turnLeft();
     } else {
--- src/distance.cpp
+++ src/distance.cpp
@@ -2,22 +2,37 @@
 
 // function to calculate distance
 int getDistance() {
+  long sum{};
+  int count{};
   long duration{};
+  long avgDuration{};
 
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  for (int i{}; i < 3; i++) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH, 25000);
-  if (duration == 0) {
-    return 999;
-  } 
-  else {
-    return duration * 0.034 / 2;
+    duration = pulseIn(echoPin, HIGH, 25000);
+
+    if (duration > 0) {
+      sum += duration;
+      count++;
+    }
+
+    delay(5);
   }
+
+  if (count == 0) {
+    return 999;
+  }
+
+  avgDuration = sum / count;
+
+  return avgDuration * 0.034 / 2;
 }
+
 
 // ---------- Servo Scan ----------
 int lookLeft() {
